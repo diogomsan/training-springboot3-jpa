183e3352d37d00bddff919a716ebf39c5e0b4950
dsantos <diogo.santos@jacto.com.br>
2025-08-17 12:58:07 -0300

    User entity and resource

---
+++ src/main/java/com/training/course/resources/UserResource.java
@@ -0,0 +1,23 @@
+package com.training.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.training.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+
+        User u = new User(1L, "diogo", "diogo@teste.com", "99999999", "123456");
+
+        return ResponseEntity.ok().body(u);
+        
+    }
+
+}
