d30bb6e3988fa3acdff830d47b1da456c882cdae
dsantos <diogo.santos@jacto.com.br>
2025-08-17 17:01:53 -0300

    Service layer, component registration

---
+++ src/main/java/com/training/course/services/UserService.java
@@ -0,0 +1,28 @@
+package com.training.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.training.course.entities.User;
+import com.training.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.get();
+
+    }
+
+}
