610bbc7daa6180e7be8e6afc3c074f53b624d2b0
dsantos <diogo.santos@jacto.com.br>
2025-08-30 10:50:47 -0300

    Product and Relationship ManyToMany Category

--- src/main/java/com/training/course/entities/Category.java
+++ src/main/java/com/training/course/entities/Category.java
@@ -1,16 +1,17 @@
 package com.training.course.entities;
 
 import java.io.Serializable;
-//import java.util.HashSet;
-//import java.util.Set;
+import java.util.HashSet;
+import java.util.Set;
 
-//import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-//import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,9 +24,9 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 
-	//@JsonIgnore
-	//@ManyToMany(mappedBy = "categories")
-	//private Set<Product> products = new HashSet<>();
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 	}
---
+++ src/main/java/com/training/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.training.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.training.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
